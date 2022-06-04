a5e518bad7839323e3c0d862f686470c8f77bec9
mrc-bsllt <mrc.bsllt@gmail.com>
2022-06-04 09:08:53 +0200

    connected app with mongoose

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
 const path = require('path')
 const express = require('express')
-const { mongoConnect } = require('./utils/database')
+const mongoose = require('mongoose')
+// const { mongoConnect } = require('./utils/database')
 
 const bodyParser = require('body-parser')
 
 // MODELS
-const User = require('./models/User')
+// const User = require('./models/User')
 
 const app = express()
 
-app.use((req, res, next) => {
-  User.getUserByEmail('mrc@mail.com').then(user => {
-    req.user = user
-    next()
-  }).catch(error => console.log(error))
-})
+// app.use((req, res, next) => {
+//   User.getUserByEmail('mrc@mail.com').then(user => {
+//     req.user = user
+//     next()
+//   }).catch(error => console.log(error))
+// })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
-const { userRoutes } = require('./routes/user')
-const { adminRoutes } = require('./routes/admin')
+// const { userRoutes } = require('./routes/user')
+// const { adminRoutes } = require('./routes/admin')
 
-app.use(userRoutes)
-app.use('/admin', adminRoutes)
+// app.use(userRoutes)
+// app.use('/admin', adminRoutes)
 app.use('/', (req, res, next) => {
   res.status(404).render('404', { path: '404' })
 })
 
-mongoConnect(() => {
-  User.fetchUsers().then(users => {
-    if(!users.length) {
-      const user = new User('mrc.bsllt', 'mrc@mail.com')
-      user.save().then(() => {
-        app.listen(3000)
-      }).catch(error => console.log(error))
-    } else {
-      app.listen(3000)
-    }
-  }).catch(error => console.log(error))
-})
+mongoose.connect('mongodb+srv://mrc-bsllt:marcodevelon@cluster0.niomo.mongodb.net/shop?retryWrites=true&w=majority').then(() => {
+  app.listen(3000)
+}).catch(error => console.log(error))
+// mongoConnect(() => {
+//   User.fetchUsers().then(users => {
+//     if(!users.length) {
+//       const user = new User('mrc.bsllt', 'mrc@mail.com')
+//       user.save().then(() => {
+//         app.listen(3000)
+//       }).catch(error => console.log(error))
+//     } else {
+//       app.listen(3000)
+//     }
+//   }).catch(error => console.log(error))
+// })
 
   
