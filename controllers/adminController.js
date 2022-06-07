598b041bb5517aed61325907272689e507b3533c
Marco Busellato <marco.busellato@develondigital.com>
2022-06-07 13:04:13 +0200

    added input type file for images

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,26 +9,38 @@ const productsPage = (req, res, next) => {
 
 // Add product POST
 const addProduct = (req, res, next) => {
-  const { title, price, image_url, description } = req.body
-  const user_id = req.session.user
+  const { title, price, description } = req.body
+  const image = req.file
+  const user_id = req.user
+  
+  if(!image) {
+    req.flash('errors', 'Image format not valid!')
+    return res.redirect('/admin/add-product')
+  }
+  
+  const image_url = '/' + image.path
   const product = new Product({ title, price, image_url, description, user_id })
 
   product.save().then(() => {
       res.redirect('/admin/products')
-    }).catch(error => console.log(error))
+    }).catch(err => {
+      const error = new Error(err)
+      error.httpStatusCode = 500
+      return next(error)
+    })
 }
 
 // GET add product form page
 const addProductPage = (req, res, next) => {
-  res.render('admin/add-product', { path: 'add-product' })
+  res.render('admin/add-product', { path: 'add-product', errors: req.flash('errors') })
 }
 
 // GET edit product page
 const editProductPage = (req, res, next) => {
   const id = req.params.id
-  
-  Product.findById(id).then(product => {
-    res.render('admin/edit-product', { product, path: 'edit-product' })
+
+  Product.findOne({ id }).then(product => {
+    res.render('admin/edit-product', { product, path: 'edit-product', errors: req.flash('errors') })
   }).catch(err => {
     const error = new Error(err)
     error.httpStatusCode = 500
@@ -37,12 +49,19 @@ const editProductPage = (req, res, next) => {
 }
 // POST edit product
 const editProduct = (req, res, next) => { 
-  const { id, title, image_url, price, description } = req.body
-  const user_id = req.session.user
+  const { id, title, price, description } = req.body
+  const image = req.file
+  const user_id = req.user
 
-  Product.findById(id).then(product => {
+  if(!image) {
+    const product = { id, title, price, description, image }
+    req.flash('errors', 'Image format not valid!')
+    return res.render('admin/edit-product', { product, path: 'edit-product', errors: req.flash('errors') })
+  }
+
+  Product.findOne({ id }).then(product => {
     product.title = title
-    product.image_url = image_url
+    product.image_url = '/' + image.path
     product.price = price
     product.description = description
     product.user_id = user_id
@@ -66,11 +85,15 @@ const deleteProduct = (req, res, next) => {
     if(isInCart) {
       req.user.removeToCart(product._id).then(() => {
         res.redirect('/admin/products')
-      }).catch(error => console.log(error))
+      }).catch(error => next(new Error(error)))
     } else {
       res.redirect('/admin/products')
     }
-  }).catch(error => console.log(error))
+  }).catch(err => {
+    const error = new Error(err)
+    error.httpStatusCode = 500
+    return next(error)
+  })
 }
 
 module.exports = { addProduct, addProductPage, editProductPage, editProduct, productsPage, deleteProduct }
--- server.js
+++ server.js
@@ -6,11 +6,30 @@ const express = require('express')
 
 const session = require('express-session')
 
+const flash = require('connect-flash')
+
 const mongoose = require('mongoose')
 const MongoDBstore = require('connect-mongodb-session')(session)
 const MONGODB_URI = 'mongodb+srv://mrc-bsllt:marcodevelon@cluster0.niomo.mongodb.net/shop'
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') // Con questo posso agilmente salvare i dati testuali nel req.body della richiesta
+
+const multer = require('multer') // Con questo posso salvare i file in formato binario
+const fileStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images')
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.filename + '-' + file.originalname)
+  }
+}) 
+const fileFilter = (req, file, callback) => {
+  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    callback(null, true)
+  } else {
+    callback(null, false)
+  }
+}
 
 const csrf = require('csurf')
 const csrfProtection = csrf()
@@ -21,8 +40,11 @@ const User = require('./models/User')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(flash())
 
 const store = new MongoDBstore({
   uri: MONGODB_URI,
@@ -69,10 +91,11 @@ app.use(authRoutes)
 app.use(userRoutes)
 app.use('/admin', adminRoutes)
 app.use('/', (req, res, next) => {
-  res.status(404).render('404', { path: '404', user: req.session.user })
+  res.status(404).render('404', { path: '404', user: req.user })
 })
 app.use((error, req, res, next) => {
-  res.status(500).render('500', { path: '500', user: req.session.user })
+  console.log(error)
+  res.status(500).render('500', { path: '500', user: req.user })
 })
 
 mongoose.connect(`${MONGODB_URI}?retryWrites=true&w=majority`).then(() => {
